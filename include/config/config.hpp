4ffb38e65909834595503d6850a96914e0d1e848
Almanfi <nassimed.dev@gmail.com>
2024-01-07 23:44:43 +0100

    _

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -6,7 +6,7 @@
 /*   By: maboulkh <maboulkh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 15:44:31 by maboulkh          #+#    #+#             */
-/*   Updated: 2024/01/07 17:46:23 by maboulkh         ###   ########.fr       */
+/*   Updated: 2024/01/07 23:34:42 by maboulkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <map>
 #include <vector>
@@ -116,9 +117,60 @@ private:
     std::vector<serverContext> server;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class location;
+class server;
+
+class Parser {
+public:
+    Parser(const std::string& filePath);
+    ~Parser();
+    bool findBracket(const std::string& bracket);
+    bool is_space(const char c);
+    bool is_delimitter(const char c);
+    bool isEndingLine();
+    std::string getToken();
+    int getLineNum();
+    std::vector<configScope>& getScopes();
+
+private:
+    std::ifstream configFile;
+    int lineNumber;
+    std::string line;
+    size_t linePos;
+    std::vector<configScope> scopes;
+};
+
 class Config {
+public:
+    Config(const string& filePath);
+    ~Config();
+    void read();
+    void print();
+    void set(const string& token);
+private:
+    void readMainContext();
+    Parser p;
     vector<server> servers;
     vector<location> locations;
+    static map<string, int> directive;
 };
 
 struct server {
@@ -126,90 +178,20 @@ struct server {
     string  listenIp;
     int     listenPort;
     string  error_page;
+    server(Parser& p);
+    ~server();
     void setServerInfo(const string& token);
     void parseListen();
+    void parseServerName();
+    const string& validateIp(const string& ip);
+    int validatePort(const string& port);
     void checkServerInfo();
-    server(ConfigParser& p);
+    map<string, location*> location; // uri, location
 private:
     static map<string, int> directive;
     map<string, string> info;
-    // map<string, location> location; // uri, location
-    ConfigParser& p;
+    Parser& p;
 };
-
-server::server(ConfigParser& p) : p(p) {
-}
-
-server::~server() {
-}
-
-map<string, int> server::directive = {
-    std::make_pair("server_name", 0),
-    std::make_pair("listen", 1),
-    std::make_pair("error_page", 1)
-};
-
-void server::setServerInfo(const string& token) {
-    map<string, int>::iterator it = directive.find(token);
-    if (it == directive.end()) {
-        throw locExp::DIRECT_NOT_VALID();
-    }
-    if (info.find(token) != info.end()) {
-        throw locExp::DIRECT_ALREADY_SET();
-    }
-    int count = 0;
-    string value = "";
-    string newToken = "";
-    while (true) {
-        newToken = p.getToken();
-        if (newToken == ";") {
-            break;
-        }
-        if (count > 1) {
-            value += " ";
-        }
-        value += newToken;
-        count++;
-    }
-    if (it->second && it->second != count) {
-        throw locExp::TOO_MANY_ARGS();
-    }
-    info[token] = value;
-}
-
-void server::parseListen() {
-    string listen = info["listen"];
-    size_t colon = listen.find(":");
-    if (colon == string::npos) {
-        listenIp = "";
-        listenPort = 80;
-        return ;
-    }
-    string ipStr = listen.substr(0, colon);
-    string portStr = listen.substr(colon + 1);
-    listenPort =  std::strtol(portStr.c_str(), NULL, 10);
-    if (listenPort < 0 || listenPort > 65535) {
-        throw locExp::NOT_VALID_PORT();
-    }
-
-}
-
-void server::checkServerInfo() {
-    if (info.find("listen") == info.end()) {
-        throw locExp::DIRECT_NOT_VALID();
-    }
-    listenIp = info["listen"].substr(0, info["listen"].find(":"));
-    listenPort = atoi(info["listen"].substr(info["listen"].find(":") + 1).c_str());
-    if (info.find("server_name") == info.end()) {
-        if (listenIp.empty())
-            server_name.push_back("127.0.0.1");
-        else
-            server_name.push_back(listenIp);
-    }
-    if (info.find("error_page") == info.end()) {
-        throw locExp::DIRECT_NOT_VALID();
-    }
-}
 
 class locExp {
 public:
@@ -237,68 +219,18 @@ public:
 
 class location : public server {
 public:
-    location(ConfigParser& p, server& serv);
+    location(Parser& p, server& serv, const string& uri);
     ~location();
-    void setServerInfo(const string& token);
     void setLocationInfo(const string& token);
-
+    void checkLocationInfo();
+    string& getUri() const;
 private:
+    string uri;
     map<string, string> info;
     static map<string, int> directive;
     server &serv;
-    ConfigParser &p;
+    Parser &p;
 };
-
-location::location(ConfigParser& p, server& serv) : p(p), serv(serv) {
-}
-
-location::~location() {
-}
-
-map<string, int> location::directive = {
-    std::make_pair("root", 1),
-    std::make_pair("index", 0),
-    std::make_pair("autoindex", 1),
-    std::make_pair("client_max_body_size", 1),
-    std::make_pair("cgi", 0),
-    std::make_pair("upload_store", 1),
-    std::make_pair("upload_pass", 1),
-    std::make_pair("upload_pass_args", 1),
-    std::make_pair("methods", 1),
-    std::make_pair("return", 1),
-    std::make_pair("auth_basic", 1),
-    std::make_pair("auth_basic_user_file", 1),
-    std::make_pair("access_log", 1)
-};
-
-void location::setLocationInfo(const string& token) {
-    map<string, int>::iterator it = directive.find(token);
-    if (it == directive.end()) {
-        throw locExp::DIRECT_NOT_VALID();
-    }
-    if (info.find(token) != info.end()) {
-        throw locExp::DIRECT_ALREADY_SET();
-    }
-    int count = 0;
-    string value = "";
-    string newToken = "";
-    while (true) {
-        newToken = p.getToken();
-        if (newToken == ";") {
-            break;
-        }
-        if (count > 1) {
-            value += " ";
-        }
-        value += newToken;
-        count++;
-    }
-    if (it->second && it->second != count) {
-        throw locExp::TOO_MANY_ARGS();
-    }
-    info[token] = value;
-}
-
 
 
 #endif // CONFIG_HPP
