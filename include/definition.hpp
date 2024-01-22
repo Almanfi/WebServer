3ce51540bc22d4d76b7f1cb4217eb7b9b1c85713
Almanfi <nassimed.dev@gmail.com>
2024-01-22 22:23:38 +0100

    uuid generation done

--- include/definition.hpp
+++ include/definition.hpp
@@ -6,7 +6,7 @@
 /*   By: maboulkh <maboulkh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 23:44:54 by maboulkh          #+#    #+#             */
-/*   Updated: 2024/01/20 03:52:21 by maboulkh         ###   ########.fr       */
+/*   Updated: 2024/01/22 22:20:34 by maboulkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <string>
 # include <cstring>
 # include <cstdlib>
+# include <ctime>
 # include <algorithm>
 # include <vector>
 # include <deque>
@@ -36,6 +37,7 @@ using std::cin;
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::ostream;
 using std::stringstream;
 using std::string;
 using std::map;
